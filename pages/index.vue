a121c15d7938e06fb43bdb7c4243df43d8294b90
Fayaz Ahmed <fayazara@gmail.com>
2021-06-24 12:28:28 +0530

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="container">
+    <nuxt-picture src="/github-features-full.jpg" />
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.container {
+  padding: 2rem;
+  margin: 0 auto;
+  max-width: 1024px;
+}
+</style>
